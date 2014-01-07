186ae97908a25ef3d498f54899ab1735c154755d
Richard Cook <rcook@rcook.org>
2014-01-06 20:51:05 -0800

    Add fixture set helper methods to TestUtils module to fix deprecation warnings

--- features/support/fixtures.rb
+++ features/support/fixtures.rb
@@ -1,8 +1,9 @@
 Before do
-  ActiveRecord::Fixtures.reset_cache
+  TestUtils.reset_fixture_cache
   fixtures_folder = File.join(Rails.root, 'test', 'fixtures')
   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map do |f|
     File.basename(f, '.yml')
   end
-  ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
+  TestUtils.create_fixtures(fixtures_folder, fixtures)
 end
+
--- test/test_utils.rb
+++ test/test_utils.rb
@@ -14,14 +14,36 @@ module TestUtils
   end
 
   def dummy_app_dir
-    major_version = if rails_version_in_range?('3.2.0', '4.0.0')
+    major_version = if rails_3?
       '3'
-    elsif rails_version_in_range?('4.0.0', '5.0.0')
+    elsif rails_4?
       '4'
     else
       raise "Unsupported Rails version #{TestUtils.rails_version}"
     end
     "dummy-rails#{major_version}"
+  end
+
+  def reset_fixture_cache
+    fixture_set.reset_cache
+  end
+
+  def create_fixtures(*args)
+    fixture_set.create_fixtures(*args)
+  end
+
+  private
+
+  def fixture_set
+    rails_3? ? ActiveRecord::Fixtures : ActiveRecord::FixtureSet
+  end
+
+  def rails_3?
+    rails_version_in_range?('3.2.0', '4.0.0')
+  end
+
+  def rails_4?
+    rails_version_in_range?('4.0.0', '5.0.0')
   end
 end
 
