db64be0a7a5a0292c89e44abbd0df1f3a33099ae
Richard Cook <rcook@rcook.org>
2014-04-08 14:48:09 -0700

    Refs #2: Add additional test for configuration of alternative user model class

--- test/functional/impersonate_controller_test.rb
+++ test/functional/impersonate_controller_test.rb
@@ -16,24 +16,50 @@ class UserImpersonate::ImpersonateControllerTest < ActionController::TestCase
     assert_redirected_to 'http://test.host/'
   end
 
-  test 'configuration to use a different model than User' do
-    # AdminUser instead of User
-    UserImpersonate::Engine.config[:current_staff]            = "current_admin_user"
-    UserImpersonate::Engine.config[:authenticate_user_method] = "authenticate_admin_user!"
+  test 'configuration uses default model User' do
+    user = mock('User')
 
-    # must get the correct configuration instead of returning the default
     @controller.expects(:config_or_default).
       with(:authenticate_user_method, 'authenticate_user!').
-      returns("authenticate_admin_user!")
+      returns('authenticate_user!')
     @controller.expects(:config_or_default).
-      with(:current_staff, "current_user").
-      returns("current_admin_user")
+      with(:current_staff, 'current_user').
+      returns('current_user')
     @controller.expects(:config_or_default).
-      with(:user_is_staff_method, "staff?").
-      returns("staff?")
-    @controller.stubs(:authenticate_admin_user!).returns(true)
-    @controller.stubs(:current_admin_user).returns(mock('User'))
-    get :index, :use_route => "user_impersonate"
+      with(:user_is_staff_method, 'staff?').
+      returns('staff?')
+
+    @controller.expects(:authenticate_user!).once.returns(true)
+    @controller.expects(:current_user).once.returns(user)
+    @controller.expects(:authenticate_admin_user!).never
+    @controller.expects(:current_admin_user).never
+
+    get :index, :use_route => 'user_impersonate'
+    assert_redirected_to 'http://test.host/'
+    assert_equal user, assigns(:current_staff)
+  end
+
+  test 'configuration to use a different model than User' do
+    admin_user = mock('AdminUser')
+
+    @controller.expects(:config_or_default).
+      with(:authenticate_user_method, 'authenticate_user!').
+      returns('authenticate_admin_user!')
+    @controller.expects(:config_or_default).
+      with(:current_staff, 'current_user').
+      returns('current_admin_user')
+    @controller.expects(:config_or_default).
+      with(:user_is_staff_method, 'staff?').
+      returns('staff?')
+
+    @controller.expects(:authenticate_user!).never
+    @controller.expects(:current_user).never
+    @controller.expects(:authenticate_admin_user!).once.returns(true)
+    @controller.expects(:current_admin_user).once.returns(admin_user)
+
+    get :index, :use_route => 'user_impersonate'
+    assert_redirected_to 'http://test.host/'
+    assert_equal admin_user, assigns(:current_staff)
   end
 end
 
